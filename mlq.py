6444b1dbdf238354505ed9e0ae06a38fc1274951
Hexogenua <tuancnttk13@gmail.com>
2019-04-09 11:04:28 +0700

    first project

---
+++ mlq.py
@@ -0,0 +1,23 @@
+import mysql.connector, ipaddress, os, whois
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="Quang123**",
+  database="ip2location"
+
+)
+mycursor = mydb.cursor()
+def abc(domain):
+    ip_address = int(ipaddress.ip_address(domain))
+    ip = "select country_name,region_name,city_name from ip2location_db5 where ip_from = {} or ip_to = {}"
+    mycursor.execute(ip.format(ip_address, ip_address))
+    myresult = mycursor.fetchall()
+    if myresult == []:
+        return "Not Found"
+    else:
+        for i in myresult:
+            b = []
+            for item in i:
+                b.append((item.decode('utf-8')))
+            c = ', '.join(b)
+        return c
